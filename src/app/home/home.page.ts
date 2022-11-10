054decfe99c37195857ac32fefd722bb289949eb
Ihsan-Zr <mohamadnurihsansyah@gmail.com>
2022-11-10 22:34:31 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public GetDataMahasiswa: any;
+
+  constructor(
+    private http: HttpClient,
+  ) {  this._Getdata();}
+
+  _Getdata() {
+    let data: Observable<any>;
+    data = this.http.get('http://localhost/apici3/api/mahasiswa/');
+    data.subscribe(result => {
+      this.GetDataMahasiswa = result;
+      console.log(result);
+      this.GetDataMahasiswa = result.data;
+    });
+  }
+
+  handleRefresh(event){
+    setTimeout(() => {
+      this._Getdata();
+      event.target.complete();
+    }, 2000);
+  };
+}
